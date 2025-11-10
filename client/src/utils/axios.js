9ca5ae53f6c05b8a0a7fd60b91cb33ab9ced746b
Dheeraj Koneti <Dheeraajkoneti2412@gmail.com>
2025-11-11 01:29:20 +0530

    Update socket CORS

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // ✅ remove quotes
+  baseURL: process.env.REACT_APP_API_URL || "https://careconnect-3-x25w.onrender.com",
+  withCredentials: true,
 });
 
 export default instance;
--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_API_URL, {
+const socket = io(process.env.REACT_APP_API_URL || "https://careconnect-3-x25w.onrender.com", {
   transports: ["websocket"],
 });
 
